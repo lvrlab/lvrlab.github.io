c38d826608124b5d70d8e3184b0e605d23fd3ffc
kkhhenry <85471646+byungokhan@users.noreply.github.com>
2024-10-03 22:43:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ window.addEventListener('load', function() {
     document.body.classList.remove('loading');
     document.body.classList.add('loaded');
 });
+
+// 햄버거 메뉴 토글 기능
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    this.classList.toggle('active');  // 햄버거 메뉴 아이콘의 상태를 변경
+    const navMenu = document.querySelector('#header-container nav');
+    if (navMenu.style.display === 'block') {
+        navMenu.style.display = 'none';  // 메뉴가 열려있다면 숨기기
+    } else {
+        navMenu.style.display = 'block';  // 메뉴가 닫혀있다면 열기
+    }
+});
